114207d9034945ad1c1da7d98d67a370896ecc53
duy-win <nguyenducduy1020304050@gmail.com>
2023-07-25 22:04:18 +0700

    first commit

---
+++ 1_test/main.js
@@ -0,0 +1,29 @@
+/* 1 so ham built-in
+alert
+console.log();
+confirm
+prompt
+setTimeout
+setInterval
+ */
+
+var fullName = 'Nguyen Duc Duy';
+
+// console.log(fullName);
+// console.error(fullName);
+// console.warn(fullName);
+
+// confirm('Ban du 18+ ?');
+
+// prompt('Moi ban nhap gia tri');
+
+// setTimeout(() => {
+//     console.log(Math.random());
+// });
+var n = 1;
+setInterval(() => {
+        // console.log(Math.random());
+        n ++;
+        console.log(n);
+    }, 1000
+)
